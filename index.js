79b4ec5434b3ca01ef393c7b3dcf7b73e3564eba
daniyalak <50395820+daniyalak@users.noreply.github.com>
2019-05-11 04:28:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ restService.post("/echo", function(req, res) {
       ? req.body.result.parameters.echoText
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech + 'hello',
-    displayText: speech + 'hello',
+    speech: speech + app.getUser().userId,
+    displayText: speech + app.getUser().userId,
     source: "webhook-echo-sample"
   });
 });
